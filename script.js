45f9ae3a8a4e8787d8dd277f18d3b1b8ba1e3859
rajeswari-industries <dhanushedits18@gmail.com>
2024-12-29 12:34:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+    // Function to open the sidebar
+    function openSidebar() {
+        document.getElementById("mySidebar").classList.add("active");
+    }
+
+    // Function to close the sidebar
+    function closeSidebar() {
+        document.getElementById("mySidebar").classList.remove("active");
+    }
